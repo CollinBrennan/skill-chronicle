394de1290a5918a22e7719bd1821b1256e120699
Collin Brennan <collinbrennan8@gmail.com>
2024-11-23 19:52:45 -0500

    add sign in and sign out

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div>Login</div>
+import SigninButton from '@/components/signin-button'
+import SignoutButton from '@/components/signout-button'
+import { auth } from '../auth'
+
+export default async function Home() {
+  const session = await auth()
+
+  return <div>{!session?.user ? <SigninButton /> : <SignoutButton />}</div>
 }
